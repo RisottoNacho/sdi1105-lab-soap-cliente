f1abc822446b37b9c1e8afaf25af2e6282049df6
uo258014 <47207829+uo258014@users.noreply.github.com>
2019-04-11 10:31:19 +0200

    SDI-SW-11.2 Servicio SOAP Cliente

---
+++ src/main/java/com/uniovi/Sdi1105LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1105LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1105LabSoapClienteApplication.class, args);
+	}
+
+}
